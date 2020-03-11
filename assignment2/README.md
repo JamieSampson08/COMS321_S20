8b392bc34a7393a1351ed97ab856d86e2c5c9677
JamieSampson08 <jsampson2117@gmail.com>
2020-03-11 12:18:44 -0500

    init as2 project

---
+++ assignment2/README.md
@@ -0,0 +1,7 @@
+# Assignment 2
+Due: April 1, 2020  
+Jamie Sampson 
+
+1. emulator_legv8.py
+2. group.txt
+3. instruction.py
---
+++ assignment2/emulator_legv8.py
@@ -0,0 +1,10 @@
+# Jamie Sampson
+import sys
+
+
+def main(argv):
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
